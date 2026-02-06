854b7fd532e9fe17df304e9ea2e82241c3e73b3b
Julia Shen <juliashen51@gmail.com>
2026-02-06 13:36:53 -0600

    game planning

--- KittyKatNo/Models/GameModels.swift
+++ KittyKatNo/Models/GameModels.swift
@@ -5,7 +5,7 @@
 //  Created by Julia Shen on 2/6/26.
 //
 
-import Foundation
+import SwiftUI
 
 enum GameType {
     case single, bot, peer, undetermined
@@ -18,4 +18,41 @@ enum GameType {
         case .undetermined: return ""
         }
     }
+}
+
+enum GamePiece: String {
+    case x, o
+    var image: Image {
+        Image(self.rawValue)
+    }
+}
+
+
+struct Player {
+    let gamePiece: GamePiece
+    var name: String
+    var moves: [Int] = []
+    var isCurrent: Bool = false
+    var isWinner: Bool {
+        for moves in Move.winningMoves {
+            if moves.allSatisfy(self.moves.contains) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+enum Move {
+    static var all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    static var winningMoves = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9]
+    ]
 }
